bebf27b62dab67eebdd6faad8e2ec2b14c909bf4
cyb3rw0lf-3D <152347536+cyb3rw0lf-3D@users.noreply.github.com>
2025-10-23 22:18:42 -0500

    Update jumpstart.sh

--- jumpstart.sh
+++ jumpstart.sh
@@ -10,8 +10,12 @@ sudo apt update -y
 sudo apt install -y qbittorrent
 sudo apt install retroarch -y
 sudo apt install flatpak -y
+bash -ci "$(wget -qO - 'https://shlink.makedeb.org/install')"
+wget -qO - 'https://proget.makedeb.org/debian-feeds/prebuilt-mpr.pub' | gpg --dearmor | sudo tee /usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg 1> /dev/null
+echo "deb [arch=all,$(dpkg --print-architecture) signed-by=/usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg] https://proget.makedeb.org prebuilt-mpr $(lsb_release -cs)" | sudo tee /etc/apt/sources.list.d/prebuilt-mpr.list
+sudo apt update -y
+sudo apt install prismlauncher -y
 flatpak --user remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub org.prismlauncher.PrismLauncher -y -v
 flatpak install flathub ru.linux_gaming.PortProton -y -v
 flatpak install flathub com.vysp3r.ProtonPlus -y -v
 flatpak install flathub com.ultimaker.cura

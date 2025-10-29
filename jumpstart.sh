0d67c1696f7dd7a5986dd6bc923f7dea31a9a32a
cyb3rw0lf-3D <152347536+cyb3rw0lf-3D@users.noreply.github.com>
2025-10-28 21:40:53 -0500

    Update jumpstart.sh

--- jumpstart.sh
+++ jumpstart.sh
@@ -47,6 +47,25 @@ unzip Pokemon_Black_Version.zip -d /home/xj439912/pokemon/
 chmod +x PokeMMO.sh
 sh PokeMMO.sh
 unzip undertale.zip -d /home/xj439912/undertale
+sudo apt update -y 
+sudo apt upgrade -y 
+sudo apt install wget tar maven -y 
+wget https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.9%2B10/OpenJDK21U-jdk_x64_linux_hotspot_21.0.9_10.tar.gz
+sudo mkdir -p /opt/java
+sudo tar -xzf OpenJDK21U-jdk_x64_linux_hotspot_21.0.9_10.tar.gz -C /opt/java && \
+sudo update-alternatives --install /usr/bin/java java /opt/java/jdk-21.0.9+10/bin/java 1 && \
+sudo update-alternatives --install /usr/bin/javac javac /opt/java/jdk-21.0.9+10/bin/javac 1 && \
+sudo update-alternatives --config java && \
+sudo update-alternatives --config javac && \
+echo 'export JAVA_HOME=/opt/java/jdk-21.0.9+10' >> ~/.bashrc && \
+echo 'export PATH=$PATH:$JAVA_HOME/bin' >> ~/.bashrc && \
+source ~/.bashrc && \
+java -version && \
+mvn -version
+wget https://github.com/cyb3rw0lf-3D/crostini-jumpstart/raw/refs/heads/main/ProjectVenom-source-1.0.0.zip
+unzip ProjectVenom-source-1.0.0.zip -d /home/xj439912/project-venom
+cd project-venom
+mvn clean package -DskipTests
 clear
 echo Setup finished! Rebooting in 5 seconds...
 sleep 5s

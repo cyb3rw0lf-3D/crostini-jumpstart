29c6f2b94c615b9f9b62850e1b12113008771570
cyb3rw0lf-3D <152347536+cyb3rw0lf-3D@users.noreply.github.com>
2025-11-05 21:37:11 -0600

    Update jumpstart.sh

--- jumpstart.sh
+++ jumpstart.sh
@@ -6,8 +6,8 @@ sudo dpkg --add-architecture i386
 sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/debian/dists/bookworm/winehq-bookworm.sources
 sudo apt update
 sudo apt install --install-recommends winehq-stable -y
-wget https://github.com/NoRiskClient/noriskclient-launcher/releases/latest/download/NoRiskClient-Linux.deb
-sudo apt install ./NoRiskClient-Linux.deb -y
+wget https://github.com/cyb3rw0lf-3D/crostini-jumpstart/raw/refs/heads/main/atlauncher-1.4-1.deb
+sudo apt install ./atlauncher-1.4-1.deb -y
 sudo apt install -y qbittorrent -y
 sudo apt install flatpak -y
 flatpak --user remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
@@ -20,43 +20,20 @@ sudo apt install kmod -y
 wget https://launchpad.net/~kxstudio-debian/+archive/kxstudio/+files/kxstudio-repos_11.2.0_all.deb
 sudo dpkg -i kxstudio-repos_11.2.0_all.deb
 sudo apt-get install lmms -y
+wget https://swr.cloud.blackmagicdesign.com/DaVinciResolve/v20.2.3/DaVinci_Resolve_20.2.3_Linux.zip?verify=1762399670-Fz9yA7bXFv%2BB8poojDIjnHsAQjHbheaIvnQS8biXWD4%3D
+unzip DaVinci_Resolve_20.2.3_Linux.zip -d /home/infernobookx/davinci
+cd davinci
+chmod +x DaVinci_Resolve_XX.Y.Z_Linux.run
+sudo ./DaVinci_Resolve_XX.Y.Z_Linux.run
 wget https://archive.org/download/hollow-knight-1.5.78.11833-linux-drmfree/Hollow_Knight_1.5.78.11833_LinuxDRMFree.zip
 unzip Hollow_Knight_1.5.78.11833_LinuxDRMFree.zip -d /mnt/chromeos/removable/devSD/hollow
 sudo rm -rf Hollow_Knight_1.5.78.11833_LinuxDRMFree.zip
 sudo apt-get install zenity -y
-sudo apt update -y
 sudo apt update -y
 sudo apt upgrade -y
 curl -L "https://go.microsoft.com/fwlink/?LinkID=760868" > /tmp/vscode.deb
 sudo dpkg -i /tmp/vscode.deb 
 sudo apt-get install -f
 sudo echo -e "[Desktop Entry]\nName=VSCode\nComment=Visual Studio Code\nExec=/opt/vscode/Code\nIcon=/opt/vscode/resources/app/resources/linux/code.png\nType=Application\nVersion=1.0\nTerminal=false\nCategories=Development" > /usr/share/applications/vscode.desktop
-curl -L https://pokemmo.com/download_file/1/ > PokeMMO.zip
-sudo unzip PokeMMO.zip /pokemmo/
-unzip Pokemon_Black_Version.zip -d /home/xj439912/pokemon/
-chmod +x PokeMMO.sh
-sh PokeMMO.sh
-sudo apt update -y 
-sudo apt upgrade -y 
-sudo apt install wget tar maven -y 
-wget https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.9%2B10/OpenJDK21U-jdk_x64_linux_hotspot_21.0.9_10.tar.gz
-sudo mkdir -p /opt/java
-sudo tar -xzf OpenJDK21U-jdk_x64_linux_hotspot_21.0.9_10.tar.gz -C /opt/java && \
-sudo update-alternatives --install /usr/bin/java java /opt/java/jdk-21.0.9+10/bin/java 1 && \
-sudo update-alternatives --install /usr/bin/javac javac /opt/java/jdk-21.0.9+10/bin/javac 1 && \
-sudo update-alternatives --config java && \
-sudo update-alternatives --config javac && \
-echo 'export JAVA_HOME=/opt/java/jdk-21.0.9+10' >> ~/.bashrc && \
-echo 'export PATH=$PATH:$JAVA_HOME/bin' >> ~/.bashrc && \
-source ~/.bashrc && \
-java -version && \
-mvn -version
-wget https://github.com/cyb3rw0lf-3D/crostini-jumpstart/raw/refs/heads/main/ProjectVenom-source-1.0.0.zip
-unzip ProjectVenom-source-1.0.0.zip -d /home/xj439912/project-venom
-cd project-venom
-mvn clean package -DskipTests
-wget https://github.com/webtorrent/webtorrent-desktop/releases/download/v0.24.0/webtorrent-desktop_0.24.0_amd64.deb
-sudo apt install ./webtorrent-desktop_0.24.0_amd64.deb -y
-echo Setup finished! Rebooting in 5 seconds...
-echo You can now also play Undertale! Execute /GOG\ Games/Undertale/start.sh to start the game up! (after getting all the dependencies, ofc! :))
 sleep 5s
+exit

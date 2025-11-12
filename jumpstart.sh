8045deb1ad835fc7b9ee92a4f801ccc5b61321c2
cyb3rw0lf-3D <152347536+cyb3rw0lf-3D@users.noreply.github.com>
2025-11-12 17:23:58 -0600

    Update jumpstart.sh

--- jumpstart.sh
+++ jumpstart.sh
@@ -29,6 +29,7 @@ wget https://archive.org/download/hollow-knight-1.5.78.11833-linux-drmfree/Hollo
 unzip Hollow_Knight_1.5.78.11833_LinuxDRMFree.zip -d /mnt/chromeos/removable/devSD/hollow
 sudo rm -rf Hollow_Knight_1.5.78.11833_LinuxDRMFree.zip
 sudo apt-get install zenity -y
+wget 
 sudo apt update -y
 sudo apt upgrade -y
 curl -L "https://go.microsoft.com/fwlink/?LinkID=760868" > /tmp/vscode.deb
@@ -43,4 +44,13 @@ Type=Application
 StartupNotify=false
 StartupWMClass=Code
 Categories=TextEditor;Development;IDE;" > /usr/share/applications/vscode.desktop'
+sudo apt update
+sudo apt install nodejs npm -y
+wget https://download1654.mediafire.com/n4inhziymkqgfOBfAAMs9Y-Mn73WcYH0UkCnJzPJFt4IhstA0qUcNVxSrdVJSGqr5Gg6P_byfQPaekaBk73i4h8fDObRbfA53Nzy2sW4h_I8wZhQIbJz4zvAxTEESfc7vU9ZwgBaldX98lQL4HqjTQzZ03RFzlLr6U0esT0dPvOL/eye8shb5mo3v5zd/Celeste_%28v1.4.0.0%29_%5BLinux%5D+%28extract.me%29.zip
+mv Celeste_(v1.4.0.0)_[Linux]_(extract.me).zip celeste.zip
+mkdir celeste
+unzip celeste.zip -d /home/infernobook/celeste
+clear
+node -v
+npm -v
 exit
